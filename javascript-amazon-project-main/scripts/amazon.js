8d8a295f4764d2e63889253c107fed114c8c813f
sriirohit <sriirohit31@gmail.com>
2025-02-17 12:55:33 -0500

    Configured amazon.js

---
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -0,0 +1,90 @@
+import {cart} from '../data/cart.js' ;
+import {products} from '../data/products.js';
+
+let producthtml = ` `;
+products.forEach((product)=>{
+    const html = ` 
+    <div class="product-container">
+     <div class="product-image-container">
+       <img class="product-image"
+         src="${product.image}">
+     </div>
+
+     <div class="product-name limit-text-to-2-lines">
+       ${product.name}
+     </div>
+
+     <div class="product-rating-container">
+       <img class="product-rating-stars"
+         src="images/ratings/rating-${product.rating.stars*10}.png">
+       <div class="product-rating-count link-primary">
+         ${product.rating.count}
+       </div>
+     </div>
+
+     <div class="product-price">
+       ${product.priceCents}
+     </div>
+
+     <div class="product-quantity-container">
+       <select>
+         <option selected value="1">1</option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         <option value="5">5</option>
+         <option value="6">6</option>
+         <option value="7">7</option>
+         <option value="8">8</option>
+         <option value="9">9</option>
+         <option value="10">10</option>
+       </select>
+     </div>
+
+     <div class="product-spacer"></div>
+
+     <div class="added-to-cart">
+       <img src="images/icons/checkmark.png">
+       Added
+     </div>
+
+      <div class="add-notify"> 
+      </div>
+
+     <button class="add-to-cart-button button-primary
+     js-add-button "data-product-id="${product.id}">
+       Add to Cart
+     </button>
+   </div>
+
+`; 
+producthtml+=html;
+});
+
+document.body.querySelector('.products-grid')
+.innerHTML = producthtml;
+
+const addelement = document.body.querySelectorAll(".js-add-button");
+const addNotfiy = document.body.querySelectorAll(".add-notify");
+
+addelement.forEach((add)=>{
+    add.addEventListener('click',()=>{
+        const productId = add.dataset.productId;
+        let matchedItem = false;
+        if(cart.length!=0){
+        cart.forEach((item) => {
+        {
+          if(item.productId===productId){
+            matchedItem=true;
+            item.quantity+=1;
+          }
+        }})};
+        if(!matchedItem){
+            cart.push({
+            productId: productId,
+            quantity : 1
+        });
+        }
+      console.log(cart);
+    });
+  });
